5a09eb3ae821b3d61a11cffc1b39f17828caba48
Emre Sevim <emre.xamarin@outlook.com>
2020-12-10 21:05:37 +0300

    ...

--- Curriculum.UI.ConsoleCore/Program.cs
+++ Curriculum.UI.ConsoleCore/Program.cs
@@ -2,32 +2,11 @@
 
 namespace Curriculum.UI.ConsoleCore
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Console.WriteLine("Vize notunu giriniz: ");
-            int vizeNotu = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Final notunu giriniz: ");
-            int finalNotu = Convert.ToInt32(Console.ReadLine());
-
-            double hesaplama = vizeNotu * 0.3 + finalNotu * 0.7;
-
-            int gecmeNotu = Convert.ToInt32(hesaplama);
-
-            if (gecmeNotu >= 60)
-            {
-                Console.WriteLine(gecmeNotu + " ile geçti.");
-            }
-            else if (gecmeNotu > 39)
-            {
-                Console.WriteLine(gecmeNotu + " ile büte kaldı.");
-            }
-            else
-            {
-                Console.WriteLine(gecmeNotu + " ile kaldı.");
-            }
+            
         }
     }
 }
