99299ebc8b4adf1c49d44cdee09c931420f3999e
Emre Sevim <emre.xamarin@outlook.com>
2020-12-19 23:03:11 +0300

    ...

--- Curriculum.Courses.CSharp/Fundamentals/DataTypes.cs
+++ Curriculum.Courses.CSharp/Fundamentals/DataTypes.cs
@@ -10,25 +10,44 @@ namespace Curriculum.Courses.CSharp.Fundamentals
     {
         public void IntegralTypes()
         {
-            // const: constant (sabit)
+            // const = constant.
 
+            // 1 byte = 8 bit (bit = 1 or 0)
+            // Example for 1 byte = 00110110
+
+            // System.Byte - 1 byte
             const byte byteMin =   0;
             const byte byteMax = 255;
 
+            // System.SByte - 1 byte
+            // s = signed.
             const sbyte sbyteMin = -128;
             const sbyte sbyteMax =  127;
 
+            // System.Int16 - 2 bytes
             const short shortMin = -32768;
             const short shortMax =  32767;
 
+            // System.UInt16 - 2 bytes
+            // u = unsigned.
             const ushort ushortMin =     0;
             const ushort ushortMax = 65535;
 
+            // System.Int32 - 4 bytes
             const int intMin = -2147483648;
             const int intMax =  2147483647;
 
+            // System.UInt32 - 4 bytes
             const uint uintMin =          0;
             const uint uintMax = 4294967295;
+
+            // System.Int64 - 8 bytes
+            const long longMin = -9223372036854775808;
+            const long longMax = 9223372036854775807;
+
+            // System.UInt64 - 8 bytes
+            const ulong ulongMin = 0;
+            const ulong ulongMax = 18446744073709551615;
         }
     }
 }
--- Curriculum.UI.ConsoleCore/Program.cs
+++ Curriculum.UI.ConsoleCore/Program.cs
@@ -6,9 +6,28 @@ namespace Curriculum.UI.ConsoleCore
     {
         public static void Main()
         {
-            // DATA TYPES (veri tipleri)
+            // OPERATÖRLER
 
+            int a, b, c, d, e;
+            a = 7;       // a = 7
+            b = a;       // b = 7
+            c = b + 1;   // c = 8
+            d = a++;     // d = 7, a = 8
+            e = ++a;     // a = 9, e = 9
 
+            Console.WriteLine("a : " + a);
+            Console.WriteLine("b : " + b);
+            Console.WriteLine("c : " + c);
+
+            Console.WriteLine("d : " + d);
+            Console.WriteLine("e : " + e);
+
+            // b = a + (b * c);
+            b = a + b * c;
+            Console.WriteLine("yeni b : " + b);
+
+            b = (a + b) * c;
+            Console.WriteLine("son b : " + b);
         }
     }
 }
